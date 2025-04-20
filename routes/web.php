b6ddb3d876760b0e0dcbc4d84483c01591571c04
DoomDevil23 <orlandos723@gmail.com>
2025-04-19 21:37:03 -0500

    routes ready to implement auth an role base access

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('terrenos', TerrenoController::class)->except(['show', 'edit']);
+/*Route::resource('terrenos', TerrenoController::class)->except(['show', 'edit']);
 Route::get('/distritos/{idProvincia}', [TerrenoController::class, 'getDistritos']);
 Route::get('/corregimientos/{idDistrito}', [TerrenoController::class, 'getCorregimientos']);
 Route::get('/comunidades/{idCorregimiento}', [TerrenoController::class, 'getComunidades']);
@@ -24,4 +24,30 @@ Route::put('/comunidades/{comunidad}', [ComunidadController::class, 'update'])->
 Route::put('/corregimientos/{id}/codigo-ubicacion', [CorregimientoController::class, 'updateCodigoUbicacion']);
 Route::get('/test-comunidad/{comunidad}', function(App\Models\Comunidad $comunidad){
     dd($comunidad);
-});
+});*/
+
+//RUTAS CON AUTH
+//VISTAS PRINCIPALES
+Route::get('/terrenos', [TerrenoController::class, 'index'])->name('terrenos.index');
+Route::get('/comunidades', [ComunidadController::class, 'index'])->name('comunidades.index');
+
+//SELECT DINAMICOS
+Route::get('/distritos/{idProvincia}', [TerrenoController::class, 'getDistritos']);
+Route::get('corregimientos/{idDistrito}', [TerrenoController::class, 'getCorregimientos']);
+Route::get('/comunidades/{idCorregimiento}', [TerrenoController::class, 'getComunidades']);
+Route::get('/provincias', [ProvinciaController::class, 'index']);
+Route::get('comunidad-info/{id}', [TerrenoController::class, 'comunidadInfo']);
+
+//RUTAS POR ROL DE ADMIN Y GERENTE
+//TERRENOS
+Route::post('/terrenos', [TerrenoController::class, 'store'])->name('terrenos.store');
+Route::put('/terrenos/{terreno}', [TerrenoController::class, 'update'])->name('terrenos.update');
+Route::delete('terrenos/{terreno}', [TerrenoController::class, 'destroy'])->name('terrenos.destroy');
+
+//COMUNIDADES
+Route::post('/comunidades', [ComunidadController::class, 'store'])->name('comunidades.store');
+Route::put('comunidades/{comunidad}', [comunidadController::class, 'update'])->name('comunidades.update');
+Route::delete('comunidades/{comunidad}', [ComunidadController::class, 'destroy'])->name('comunidades.destroy');
+
+//CORREGIMIENTOS
+Route::put('/corregimientos/{id}/codigo-ubicacion', [CorregimientoController::class, 'updateCodigoUbicacion']);
