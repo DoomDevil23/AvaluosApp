9d03d01dbae32e22c3fdea4117f93ce12d9d23d1
DoomDevil23 <orlandos723@gmail.com>
2025-04-24 03:24:52 -0500

    Updated component attributes setting

--- resources/views/components/select-buscar.blade.php
+++ resources/views/components/select-buscar.blade.php
@@ -2,6 +2,7 @@
     <select 
         id="{{ $id }}" 
         name="{{ $name }}" 
+        title="{{ $title ?? "" }}"
         {{ $attributes->merge(['class' => 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 h-8 px-2 py-1 text-sm']) }}
     >
         <option value="">{{ $placeholder ?? 'Seleccionar' }}</option>
